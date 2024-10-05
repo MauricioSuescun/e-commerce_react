71a255507c5f72535eea6ce99a847e96da063c1c
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2024-10-04 23:32:59 -0500

    search function working

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <>
       <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <Navbar />
+        <SearchBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/collection" element={<Collection />} />
